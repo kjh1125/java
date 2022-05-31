0578ae28cfc2a0ca3a9fb3597c60980ed3feabbe
kjh1125 <106057481+kjh1125@users.noreply.github.com>
2022-05-31 18:14:04 +0900

    Add files via upload

---
+++ java2/Array1/src/Array11.java
@@ -0,0 +1,19 @@
+
+public class Array11 {
+
+	public static void main(String[] args) {
+		/*
+		 * 다음 배열 값이 있습니다. 해당 배열 값을 비교하여 사용자 정보를 출력하시오.
+		 * 
+		 * Adata = {"hong","kim","park","jang","lee","soun"};
+		 * Bdata = {"80","100","46","38","65","88"};
+		 * 
+		 * 결과 배열 조건은 60점이상(합격자)만 출력.
+		 * 결과 배열[hong,kim,lee,soun]
+		 * 
+		 * 
+		 */
+
+	}
+
+}
